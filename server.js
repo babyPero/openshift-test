7e5a71b6880604115cb02579e8acf05c785963f9
Maria Hirota <mhirota@dhcp-65-165.sin.redhat.com>
2018-02-05 16:56:53 +0800

    add mongoDB to be saved in mLab

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
 const bodyParser= require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+
+var db
 
 app.use(bodyParser.urlencoded({extended: true}))
 
-
-
+    /*
 app.listen(3000, function() {
 	console.log('listening on 3000')
 	    })
+    */
 
 /*
 app.get('/', function(request, response) {
@@ -16,12 +19,28 @@ app.get('/', function(request, response) {
 	    })
 */
 
-
 app.get('/', (req, res) => {
 	    res.sendFile(__dirname + '/index.html')
     })
 
 app.post('/quotes', (req, res) => {
+	db.collection('quotes').save(req.body, (err, result) => {
+		if (err) return console.log(err)
+			     console.log('saved to database')
+			     res.redirect('/')
+			     })
+
+	/*
 	    console.log('Hellooooooooooooooooo!')
 	    console.log(req.body)
+	*/
     })
+
+MongoClient.connect('mongodb://babyPero:testtest@ds217898.mlab.com:17898/test-project', (err, client) => {
+	if (err) return console.log(err)
+		     db = client.db('test-project')
+		     app.listen(3000, () => {
+		     console.log('listening on 3000')
+			 })
+
+	})
